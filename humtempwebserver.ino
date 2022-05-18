0678be63ffe8443bf48aeffad111e3eaf7eba8f4
GarrettJhnsn <45772450+GarrettJhnsn@users.noreply.github.com>
2022-05-18 08:59:09 -0700

    OLED
    Updated OLED  degree symbol with correct CP437 character

--- humtempwebserver.ino
+++ humtempwebserver.ino
@@ -16,11 +16,12 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 const char* ssid = "CheeseyNudBuns";
 const char* password = "chatta95!!";
 
-// Digital pin connected to the DHT sensor
-#define DHTPIN 4     
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
 
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -28,21 +29,31 @@ DHT dht(DHTPIN, DHTTYPE);
 AsyncWebServer server(80);
 
 String readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  //float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
   float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
   if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
   else {
+    Serial.println(t);
     return String(t);
   }
 }
 
 String readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
   else {
+    Serial.println(h);
     return String(h);
   }
 }
@@ -120,7 +131,6 @@ String processor(const String& var){
 }
 
 void setup(){
-
   Serial.begin(115200);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -172,7 +182,7 @@ void loop(){
   display.print(" ");
   display.setTextSize(1);
   display.cp437(true);
-  display.write(167);
+  display.write(248);
   display.setTextSize(2);
   display.print("F");
 
@@ -186,4 +196,7 @@ void loop(){
   
   display.display();
 
+  
+
+  
 }
